35447e32ee3d598b89945729daad56ea3f02275b
Larry Gregory <larry.gregory@elastic.co>
2019-03-14 13:01:27 -0400

    Revert "target 6.x"
    This reverts commit b610285291b5c29d7c4b03319aa70d4a9e0f9290.

--- config/config.go
+++ config/config.go
@@ -6,15 +6,9 @@ package config
 import "time"
 
 type Config struct {
-	Poll            time.Duration `config:"poll"`
-	StatePath       string        `config:"state_path"`
-	HomeSeerLogPath string        `config:"homeseer_log_path"`
-	LogBatchSize    int           `config:"log_batch_size"`
+	Period time.Duration `config:"period"`
 }
 
 var DefaultConfig = Config{
-	Poll:            5 * time.Second,
-	StatePath:       "./homeseerbeat_state.json",
-	HomeSeerLogPath: "/usr/local/HomeSeer/Data/HomeSeerLog.hsd",
-	LogBatchSize:    1000,
+	Period: 1 * time.Second,
 }
